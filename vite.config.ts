9917ca404c289b569c542727474450a86e7d3274
Dmitry Lekarev <dmitry.lekarev@gmail.com>
2026-04-24 16:50:58 +0500

    support gh-pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+	base: "/blast-game/",
+
 	resolve: {
 		alias: {
 			src: resolve(__dirname, "src"),
